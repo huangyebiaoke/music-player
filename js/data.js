40ecc864f89b76ae0874aae6341cd58ee969ac05
huangyebiaoke <huangyebiaoke@outlook.com>
2017-12-04 00:19:47 +0800

    updata 2017/12/4

---
+++ js/data.js
@@ -0,0 +1,13 @@
+/*这是一个储存音乐文件用的数组
+*/
+
+var music=new Array("Adele-Rolling-In-The-Deep",
+	"Activ-Doar-Cu-Tine",
+	"Aash-Mehta-Clsr-Aash-Mehta-Flip",
+	"a_hisa-Town-of-Windmill",
+	"Approaching-Nirvana-You",
+	"Anan-Ryoko-Refrain",
+	"Alyssa-Reid-Hurricane",
+	"Alan-Walker-Spectre",
+	"Alternative-ending-チルノのパーフェクトさんすう教室"
+	)
